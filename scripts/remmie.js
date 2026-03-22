f9620648f99c7ae79472750be132b882a55ce286
rudy <164943448+poolfl0aties@users.noreply.github.com>
2026-03-22 15:14:16 +0000

    more thingssss

--- scripts/remmie.js
+++ scripts/remmie.js
@@ -1,7 +1,8 @@
 function nextPage() {
     const paras = document.getElementsByClassName('text');
+    const pokeball = document.getElementById('pokeball');
 
     while(paras[0]) {
-    paras[0].parentNode.removeChild(paras[0]);
+        paras[0].parentNode.removeChild(paras[0]);
     }
 }
