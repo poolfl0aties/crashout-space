b7b6e4df0133e3a05319acc4fa599f7db74dc0b9
rudy <164943448+poolfl0aties@users.noreply.github.com>
2026-07-09 14:20:54 +0100

    Update identity.js

--- tools/templates/identity/identity.js
+++ tools/templates/identity/identity.js
@@ -19,7 +19,7 @@ function resizeInstance(instance){
 }
 
 window.onload = resizeAllGridItems();
-window.onload = filterItem("all");
+window.onload = filterItem("item");
 window.addEventListener("resize", resizeAllGridItems);
 
 allItems = document.getElementsByClassName("item");
@@ -30,7 +30,6 @@ for(x=0;x<allItems.length;x++){
 function filterItem(filter) {
   var x, i;
   x = document.getElementsByClassName("item");
-  if (filter == "all") filter = "item";
   console.log(filter);
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
