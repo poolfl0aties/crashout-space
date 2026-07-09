c2ae13918b96d92357390b085c73cce1d78f89a8
rudy <164943448+poolfl0aties@users.noreply.github.com>
2026-07-09 12:36:02 +0100

    Update identity.js

--- tools/templates/identity/identity.js
+++ tools/templates/identity/identity.js
@@ -32,8 +32,8 @@ function filterItem(filter) {
     console.log("filter");
     for(i=0 ; i < filterArray.length ; i++) { // for the number of items in the filter list
         if(filterArray[i] !== filter) { // if the item selected doesn't match the input filter
-            let object = filterArray[i];
-            let objectArray = document.getElementsByClassName(object); // get all the elements with the class
+            let object = filterArray[i]; // let the current class = the array number
+            let objectArray = objectArray + document.getElementsByClassName(object); // get all the elements with the class
             for(i=0 ; i < objectArray.length ; i++ ) {
                 objectArray[i].classList.add("hidden");
             }
