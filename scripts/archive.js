439d963b225ecb7ed21244409154a4b3bf068ed0
rudy <164943448+poolfl0aties@users.noreply.github.com>
2026-07-12 19:51:46 +0100

    flags

---
+++ scripts/archive.js
@@ -0,0 +1,64 @@
+function resizeGridItem(item){
+  grid = document.getElementsByClassName("grid")[0];
+  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.style.gridRowEnd = "span "+rowSpan;
+}
+
+function resizeAllGridItems(){
+  allItems = document.getElementsByClassName("item");
+  for(x=0;x<allItems.length;x++){
+    resizeGridItem(allItems[x]);
+  }
+}
+
+function resizeInstance(instance){
+	item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+window.onload = filterItem("item");
+window.onload = resizeAllGridItems(), 
+setTimeout(() => {
+  resizeAllGridItems();
+}, 10); // gives a quick delay to make sure content has loaded properly 
+window.addEventListener("resize", resizeAllGridItems);
+
+allItems = document.getElementsByClassName("item");
+for(x=0;x<allItems.length;x++){
+  imagesLoaded( allItems[x], resizeInstance);
+}
+
+function filterItem(filter) {
+  var x, i;
+  x = document.getElementsByClassName("item");
+  console.log(filter);
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    console.log(x[i].classList);
+    if (x[i].classList.contains(filter)) addClass(x[i], "show");
+  }
+  resizeAllGridItems();
+}
+
+// Show filtered elements
+function addClass(element, name) {
+  console.log(element);
+  element.classList.add(name); 
+  console.log(element.classList);
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
